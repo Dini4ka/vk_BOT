0bfc9db354e3229c39fe87ed67f6ebd95505e885
Denis <boss.karelov@gmail.com>
2022-11-21 11:43:39 +0300

    организация отправки сообщений в чат

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-import time
+from config import *
+import vk_api,vk
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.utils import get_random_id
 
-from selenium import webdriver
+def write_msg(sender,message):
+    auth.method('messages.send',{'chat_id':sender,'message': message, 'random_id': get_random_id()})
 
-driver = webdriver.Chrome()
-driver.get('https://2ip.ru')
-time.sleep(10)
-driver.close()
+auth = vk_api.VkApi(token=token)
+longpoll = VkBotLongPoll(auth,group_id=group_id)
+
+for event in longpoll.listen():
+    if event.type == VkBotEventType.MESSAGE_NEW and event.from_chat and event.message.get('text') != '':
+        reseived_message = event.message.get('text')
+        sender = event.chat_id
+        if reseived_message == 'Ты здесь ?':
+            write_msg(sender,'Здесь')
+
+
