d027d3310e2516069448a74935262f01a51606a3
Denis <boss.karelov@gmail.com>
2022-11-21 16:08:23 +0300

    1) Добавлен метод чтения сообщений 2) Бот умеет отвечать на сообщения 3) Бот умеет закреплять чужие сообщения 4) Бот умеет закреплять сое сообщение

--- main.py
+++ main.py
@@ -9,15 +9,28 @@ def write_msg(sender, message):
     auth.method('messages.send', {'chat_id': sender, 'message': message, 'random_id': get_random_id()})
 
 
+def getByConversationMessageId(peer_id, conversation_message_ids, group_id):
+    return auth.method('messages.getByConversationMessageId', {'peer_id': peer_id,
+                                                               'conversation_message_ids': conversation_message_ids,
+                                                               'group_id': group_id})
+
+
+def pin_msg(peer_id,conversation_message_ids):
+    auth.method('messages.pin',{'peer_id': peer_id,
+                                'conversation_message_id': conversation_message_ids})
+
 auth = vk_api.VkApi(token=token)
 longpoll = VkBotLongPoll(auth, group_id=group_id)
+
+
 
 for event in longpoll.listen():
     if event.type == VkBotEventType.MESSAGE_NEW and event.from_chat and event.message.get('text') != '':
         text_message = event.message.get('text')
-        if 'bot_help' in text_message:
+        if text_message:
             conv_msg_id = event.obj['message']['conversation_message_id']
             peer_id = event.obj['message']['peer_id']
-            auth.method('messages.pin',{'peer_id': peer_id, 'conversation_message_id': conv_msg_id})
-            sender=event.chat_id
-            write_msg(sender,'Закрепил сообщение')
+            sender = event.chat_id
+            write_msg(sender, text_message)
+            bot_conv_msg_id = conv_msg_id +1
+            pin_msg(peer_id,bot_conv_msg_id)
