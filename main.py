a79bc9288fc3f208a052cbaf98c75580588498a0
Denis <boss.karelov@gmail.com>
2022-11-21 15:07:58 +0300

    организация закрепления сообщения

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from config import *
-import vk_api,vk
+import vk_api, vk
+from pprint import pprint
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.utils import get_random_id
 
-def write_msg(sender,message):
-    auth.method('messages.send',{'chat_id':sender,'message': message, 'random_id': get_random_id()})
+
+def write_msg(sender, message):
+    auth.method('messages.send', {'chat_id': sender, 'message': message, 'random_id': get_random_id()})
+
 
 auth = vk_api.VkApi(token=token)
-longpoll = VkBotLongPoll(auth,group_id=group_id)
+longpoll = VkBotLongPoll(auth, group_id=group_id)
 
 for event in longpoll.listen():
     if event.type == VkBotEventType.MESSAGE_NEW and event.from_chat and event.message.get('text') != '':
-        reseived_message = event.message.get('text')
-        sender = event.chat_id
-        if reseived_message == 'Ты здесь ?':
-            write_msg(sender,'Здесь')
-
-
+        text_message = event.message.get('text')
+        if 'bot_help' in text_message:
+            conv_msg_id = event.obj['message']['conversation_message_id']
+            peer_id = event.obj['message']['peer_id']
+            auth.method('messages.pin',{'peer_id': peer_id, 'conversation_message_id': conv_msg_id})
+            sender=event.chat_id
+            write_msg(sender,'Закрепил сообщение')
