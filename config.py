8d9a968f41d176f601739cc9583dbed3c2c0e996
Denis <boss.karelov@gmail.com>
2022-11-28 13:38:49 +0300

    Добавлена функция редактирования сообщения

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-token = 'vk1.a.SNSbE_Da8_EcoQl9VbfyAGkMBuzvVHBYq47WblKBWllYqZRlqeVkakEyUoRJnlh3lODD00fQOwrfFctDuRi1_m3GhcdPnPyZKB7sHQWDt5A6IK0Z-I3JozTEbzZTcCFhQaCU1kItgkusNxpWV_NeNScWFHh77yFMghQ5Xmf-DQBrh-dm6lSajS0IflHWm0ZTBXbRr6lBvWTS4pH9p9kkCw'
-group_id = 191394468 # your group_id
-admin_id = 523017940 # admin of chat (only one person now)
+token = 'vk1.a.-I3JozTE'
+group_id = 19234234 # your group_id
+admin_id = 11111111 # admin of chat (only one person now)
